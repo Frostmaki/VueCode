79cdb98a33cb1d5eb8aa4ca34ca7cee7ceac4f41
Frostmaki <2453819476@qq.com>
2018-05-27 22:38:04 +0800

    study one day

---
+++ app/public/js/app.js
@@ -0,0 +1,44 @@
+var app=new Vue({
+    el: '#app',
+    data: {
+        name: null,
+        age: null,
+
+        foodList:[
+            {
+                name:'葱',
+                price:'10',
+                discount:0.5,
+            },
+            {
+                name:'蒜',
+                price:'4',
+                discount:1,
+            },
+            {
+                name:'姜',
+                price:'2.5',
+
+            }
+        ],
+        url:'http://www.baidu.com',
+        img:'https://dummyimage.com/300.png/09f/fff',
+
+        isActive: true,
+    },
+    methods:{
+        onclick:function () {
+            console.log('click');
+        },
+        onEnter:function () {
+            console.log('mouse Enter');
+        },
+        onOut:function () {
+            console.log('mouse Leave');
+        },
+        onSubmit:function () {
+            console.log('submited');
+        }
+
+    }
+});
