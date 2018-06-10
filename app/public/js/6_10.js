f945d9a5dac521e2d66225e605caa65d3bcc2e4e
Frostmaki <2453819476@qq.com>
2018-06-10 23:25:25 +0800

    add vue component

--- app/public/js/6_10.js
+++ app/public/js/6_10.js
@@ -2,7 +2,7 @@ var vm=new Vue({
     el:'#app',
     data:{
         message:'hello,welcome to vue!!! ',
-        foodList:'hrll',
+        
         object:[
            {
                'uId':0,
@@ -15,9 +15,52 @@ var vm=new Vue({
                'password':222222,
            },
        ],
+		count:0,
+		name:'vue.js',
 
     },
     methods:{
-
+		greet:function(event){
+			alert('hello,'+this.name+'!');
+				if(event){
+				//event是原生DOM事件
+				alert(event.target.tagName);
+			}
+			console.log(event);
+		},
+		greet2:function(event){
+			alert('hello,'+this.name+'!');
+			console.log('操一次');
+			console.log(event);
+		},
+		parameterPassing:function(message){
+			console.log(message);
+		}
     }
+});
+
+
+Vue.component('button-counter',{
+	data:function(){
+		return {
+			count:0,
+		}
+	},
+	template:'<button v-on:click="count++">button click count:{{count}} times</button>',
+});
+
+/*
+Vue.component('button-counter', {
+	data:function(){
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+*/
+
+
+new Vue({
+	el:'#component-demo',
 });
